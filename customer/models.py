910bee75901aedc8b876639e74f150555c487713
VictorGM01 <victormarques8801@gmail.com>
2022-02-02 20:19:57 -0300

    Cria modelo Customer e registra-o na página de admin

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.TextField(max_length=40)
+    last_name = models.TextField(max_length=40)
+    email = models.EmailField()
+    birth_date = models.DateField()
+    area_code = models.CharField(max_length=3)
+    phone_number = models.CharField(max_length=9)
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    created_date = models.DateTimeField(auto_now_add=True)  # Django armazena automaticamente a data de criação
+    updated_date = models.DateTimeField(auto_now=True)  # Armazena data caso haja alteração
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        db_table = 'customer'  # Nomeia tabela
