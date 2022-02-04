c68080f3f1b8ec948bd6facec0e91365bd0e30df
VictorGM01 <victormarques8801@gmail.com>
2022-02-04 20:04:22 -0300

    Personaliza nome dos campos do formulário

--- customer/forms.py
+++ customer/forms.py
@@ -3,15 +3,15 @@ from .models import Customer
 
 
 class CustomerForm(forms.ModelForm):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    birth_date = forms.DateField()
-    area_code = forms.CharField()
-    phone_number = forms.CharField()
-    country = forms.CharField()
-    state = forms.CharField()
-    city = forms.CharField()
+    first_name = forms.CharField(label="Nome")
+    last_name = forms.CharField(label="Sobrenome")
+    email = forms.EmailField(label="E-mail")
+    birth_date = forms.DateField(label="Data de Nascimento")
+    area_code = forms.CharField(label="DDD")
+    phone_number = forms.CharField(label="Telefone")
+    country = forms.CharField(label="País")
+    state = forms.CharField(label="Estado")
+    city = forms.CharField(label="Cidade")
 
     class Meta:
         model = Customer
