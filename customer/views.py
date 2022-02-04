a4a258888f73b0c905e769bbb58da43dac7045c9
VictorGM01 <victormarques8801@gmail.com>
2022-02-04 20:10:28 -0300

    Adiciona redirecionamento para a página de listagem de clientes

--- customer/views.py
+++ customer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import Customer
 from .forms import CustomerForm
+from django.urls import reverse
 
 
 class CustomerListView(ListView):
@@ -16,3 +17,6 @@ class CustomerCreateView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('customer:customer-list')
