713530eef90cb0e5e34a19ab9997cfb73f5cedbc
VictorGM01 <victormarques8801@gmail.com>
2022-02-07 13:36:33 -0300

    Cria autenticação para login

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def submit_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            auth_login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Usuário e/ou senha inválidos. Tente novamente')
+
+    return redirect('login')
